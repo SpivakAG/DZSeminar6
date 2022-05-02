28f4436a2efbd1afd1f28435465966aa4427b810
ArtemSpivak <spivak25@yandex.ru>
2022-05-02 10:41:20 +0300

    задача решена

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,1 +1,25 @@
 ﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите длину массива");
+
+int length = Convert.ToInt32(Console.ReadLine());
+if (length <= 0) Console.WriteLine("Длина массива не может быть отрицательной или равняться нулю");
+else
+{
+    int[] array = new int[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+
+        if (array[i] > 0) count++;
+    }
+
+    Console.WriteLine($"Количество положительных чисел равно {count}");
+}
